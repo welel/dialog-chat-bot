c57281d9df05c34d999dc3e3e0177f57794ac81a
Pavel Loginov <56807054+welel@users.noreply.github.com>
2024-02-17 19:37:22 +0300

    feat: send errors to bot on debug mode on

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,6 +1,7 @@
 from aiogram import Bot, F, Router
 from aiogram.types import Message
 
+from config import configs
 from models import TelegramDialogManager, DictDialogStorage
 from services.audio import save_voice_as_mp3
 from services.openai_api import audio_to_text
@@ -14,25 +15,35 @@ dialog_manager = TelegramDialogManager(DictDialogStorage())
 @router.message(F.content_type == "text")
 async def process_text_message(message: Message):
     """Gets text update and sends answer of an Open AI chatbot model."""
-    if not message.text:
-        answer = get_message(SystemMessage.NO_INPUT)
-    else:
-        answer = await dialog_manager.chat(
-            message.from_user.id, message.chat.id, message.text
-        )
-    await message.reply(text=answer)
+    try:
+        if not message.text:
+            answer = get_message(SystemMessage.NO_INPUT)
+        else:
+            answer = await dialog_manager.chat(
+                message.from_user.id, message.chat.id, message.text
+            )
+        await message.reply(text=answer)
+    except Exception as e:
+        if configs.tg_bot.debug_mode:
+            await message.reply(text=f"Error: {e}")
+        raise
 
 
 @router.message(F.content_type == "voice")
 async def process_voice_message(message: Message, bot: Bot):
     """Gets audio update and sends answer of an Open AI chatbot model."""
-    voice_path = await save_voice_as_mp3(bot, message.voice)
-    transcripted_voice_text = await audio_to_text(voice_path)
+    try:
+        voice_path = await save_voice_as_mp3(bot, message.voice)
+        transcripted_voice_text = await audio_to_text(voice_path)
 
-    if transcripted_voice_text:
-        answer = await dialog_manager.chat(
-            message.from_user.id, message.chat.id, transcripted_voice_text
-        )
-    else:
-        answer = get_message(SystemMessage.UNINTELLIGIBLE_VOICE_INPUT)
-    await message.reply(text=answer)
+        if transcripted_voice_text:
+            answer = await dialog_manager.chat(
+                message.from_user.id, message.chat.id, transcripted_voice_text
+            )
+        else:
+            answer = get_message(SystemMessage.UNINTELLIGIBLE_VOICE_INPUT)
+        await message.reply(text=answer)
+    except Exception as e:
+        if configs.tg_bot.debug_mode:
+            await message.reply(text=f"Error: {e}")
+        raise
