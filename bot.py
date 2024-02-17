b17738c87804bd02f173c659b1b786c2ccd18fc7
Pavel Loginov <56807054+welel@users.noreply.github.com>
2024-02-17 19:19:31 +0300

    fix: deprecated bot param

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
 
 from config import configs
 from handlers import user_handlers
@@ -17,9 +18,12 @@ async def main():
         "[%(asctime)s] - %(name)s - %(message)s",
     )
 
-    logger.info("Starting bot")
+    logger.info("Starting bot...")
 
-    bot: Bot = Bot(token=configs.tg_bot.token, parse_mode="HTML")
+    bot: Bot = Bot(
+        token=configs.tg_bot.token,
+        default=DefaultBotProperties(parse_mode="HTML"),
+    )
     dp: Dispatcher = Dispatcher()
 
     dp.include_router(user_handlers.router)
@@ -32,4 +36,4 @@ if __name__ == "__main__":
     try:
         asyncio.run(main())
     except (KeyboardInterrupt, SystemExit):
-        logger.info("Bot stopped")
+        logger.info("Bot stopped.")
